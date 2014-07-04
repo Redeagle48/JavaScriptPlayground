3e5dd0e82c3dd12174183ae27403c25ee5fab902
Redeagle48 <antoniopereira350@gmail.com>
2014-07-04 12:08:47 +0100

    Arrays

--- Arrays/Arrays.js
+++ Arrays/Arrays.js
@@ -1,2 +1,59 @@
 var numbers = [];
 print(numbers.length);
+
+var numbers2 = [1,2,3];
+print(numbers2.length);
+
+var numbers3 = new Array(1,2);
+print(numbers3.length);
+print(numbers3[0]);
+
+var numbers4 = new Array(10);
+print(numbers4.length);
+
+var objects = [1,"Alan",true,null];
+print(objects.length);
+
+var number = 3;
+var arr= [7,4,1776];
+print(Array.isArray(number));
+print(Array.isArray(arr));
+
+var nums = [];
+for(var i = 0; i < 100; i++) {
+    nums[i] = i+1;
+    print(nums[i]);
+}
+
+var sentence ="the quick brown fox jumped over the lazy dog";
+var words = sentence.split(" ");
+print(words);
+
+var nums2 = [];
+for(var i = 0; i < 100; i++){
+    nums2[i] = i+1;
+}
+var samenums = nums2; // copy made by reference
+nums2[0] = 400;
+print(samenums[0]);
+nums2[0] = 401;
+print(samenums[0]);
+
+var names = ["Ana", "Pedro", "António", "Rita"];
+putstr("Enter a name to search for: ");
+var name = readline();
+var position = names.indexOf(name);
+if(position >= 0){
+    print("Found " + name + " at position " + position);
+} else {
+    print("Name " + name + " not found");
+}
+
+var nums = [2,3,4,5];
+print(nums);
+var newnum = 1;
+nums.unshift(newnum);
+print(nums);
+nums = [3,4,5];
+nums.unshift(newnum,1,2);
+print(nums);
